7cdf0de51e6105d2c9de7abd55d7b77a367805bd
Rakshaa Viswanathan <46165429+rakshaa2000@users.noreply.github.com>
2026-05-14 16:26:15 +0530

    Add: buy-and-sell-crypto-with-cooldown - submission-1

---
+++ Data Structures & Algorithms/buy-and-sell-crypto-with-cooldown/submission-1.py
@@ -0,0 +1,16 @@
+class Solution:
+    def maxProfit(self, prices: List[int]) -> int:
+        cache = {}
+        def dp(index, buy):
+            if index >= len(prices):
+                return 0
+            if (index, buy) in cache:
+                return cache[(index, buy)]
+            if buy:
+                profitNow = -prices[index] + dp(index+1, not buy)
+            else:
+                profitNow = prices[index] + dp(index+2, not buy)
+            profitLater = dp(index+1, buy)
+            cache[(index, buy)] = max(profitNow, profitLater)
+            return cache[(index, buy)]
+        return dp(0, True)
